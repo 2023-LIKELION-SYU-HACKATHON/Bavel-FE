83d9f6ce1591278430f4a9679df0fc5c3b0edfa2
leey00nsu <dbstndla1212@naver.com>
2023-08-02 10:17:14 +0900

    #1 daisyUI 설치 daisyUI는 tailwindCSS 기반 컴포넌트 라이브러리로 , 구현하기 귀찮은 디자인 요소들이 구현되어있어 사용합니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'bavel-orange': 'FF7A00',
+      'bavel-gray-light': 'F5F5F5',
+      'bavel-gray-deep': '606060',
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
