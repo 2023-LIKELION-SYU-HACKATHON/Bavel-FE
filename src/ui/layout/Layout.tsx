e4e2c9b045a033d3ebe2111a3a470c1c7da9d0e1
leey00nsu <dbstndla1212@naver.com>
2023-08-12 20:20:26 +0900

    #1 TrendNavigation 클릭 시 현재 트렌드 애니메이션 추가

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col w-screen min-h-screen bg-white">
+    <div className="flex flex-col w-screen h-full min-h-screen bg-white">
       <Header />
       {children}
       <Footer />
