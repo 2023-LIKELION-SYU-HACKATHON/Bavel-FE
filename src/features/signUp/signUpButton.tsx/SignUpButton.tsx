006430a67e491f9833bad076378b14c28ae66b80
Karmesin924 <jinhong0924@naver.com>
2023-08-17 13:11:25 +0900

    #13 Cancel 버튼 뒤로가기 기능 추가

--- src/features/signUp/signUpButton.tsx/SignUpButton.tsx
+++ src/features/signUp/signUpButton.tsx/SignUpButton.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 const SignUpButton = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex gap-2">
       <button
-        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          navigate(-1);
+        }}
         className="font-semibold text-black border-2 btn w-1/2 border-black bg-white hover:bg-white hover:border-black"
       >
         Cancel
