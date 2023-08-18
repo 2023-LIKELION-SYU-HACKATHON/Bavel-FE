fd22f7b2be8482fdfb5c379feb97465f7eb249d7
Karmesin924 <jinhong0924@naver.com>
2023-08-18 22:21:12 +0900

    #13 게시글 작성 페이지 카테고리/작성하기 버튼 구현 및 API 연동

--- src/ui/input/contentInput/ContentInput.tsx
+++ src/ui/input/contentInput/ContentInput.tsx
@@ -1,8 +1,16 @@
-const ContentInput = () => {
+interface ContentInputProps {
+  setContent: (value: string) => void;
+}
+
+const ContentInput = ({ setContent }: ContentInputProps) => {
+  const handleChangeContent = (e: any) => {
+    setContent(e.target.value);
+  };
   return (
     <textarea
       className="w-full grow text-base text-black break-words bg-white resize-none "
       placeholder="내용을 입력할 수 있어요."
+      onChange={handleChangeContent}
     />
   );
 };
--- src/ui/input/titleInput/TitleInput.tsx
+++ src/ui/input/titleInput/TitleInput.tsx
@@ -1,7 +1,15 @@
-const TitleInput = () => {
+interface TitleInputProps {
+  setTitle: (value: string) => void;
+}
+
+const TitleInput = ({ setTitle }: TitleInputProps) => {
+  const handleChangeTitle = (e: any) => {
+    setTitle(e.target.value);
+  };
   return (
     <input
       type="text"
+      onChange={handleChangeTitle}
       className="bg-white text-lg w-full text-black"
       placeholder="제목을 적는 칸입니다."
     ></input>
