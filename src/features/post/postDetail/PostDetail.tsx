5ecf929399e589e5daaee8353f3e17c4ba806f13
leey00nsu <dbstndla1212@naver.com>
2023-08-18 23:32:23 +0900

    mock

--- src/features/post/postDetail/PostDetail.tsx
+++ src/features/post/postDetail/PostDetail.tsx
@@ -1,16 +1,33 @@
 import { PostBox, CommentBoxList } from '@/features/post';
 import { useParams } from 'react-router-dom';
-import { dummyKoreanPosts } from '@/mocks/post.mock';
+import {
+  dummyKoreanPosts,
+  dummyEnglishPosts,
+  dummyJapanesePosts,
+} from '@/mocks/post.mock';
 import { NewComment } from '../newComment';
 
 const PostDetail = () => {
+  const userLanguage = sessionStorage.getItem('language');
   const postId = Number(useParams().postId);
-  const dummyPost = dummyKoreanPosts.filter(post => post.id === postId)[0];
+
+  let dummyPosts = dummyKoreanPosts.filter(post => post.id === postId)[0];
+
+  if (userLanguage === 'ko') {
+    dummyPosts = dummyKoreanPosts.filter(post => post.id === postId)[0];
+  }
+  if (userLanguage === 'en') {
+    dummyPosts = dummyEnglishPosts.filter(post => post.id === postId)[0];
+  }
+  if (userLanguage === 'ja') {
+    dummyPosts = dummyJapanesePosts.filter(post => post.id === postId)[0];
+  }
+
   return (
     <>
-      <PostBox {...dummyPost} full />
+      <PostBox {...dummyPosts} full />
       <NewComment postId={postId} />
-      <CommentBoxList comments={dummyPost.comments} />
+      <CommentBoxList comments={dummyPosts.comments} />
     </>
   );
 };
